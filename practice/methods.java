d86f15c23dfb863ff4ca7b9b6cc6605709f9c45c
sundararaman28 <sundararaman.s28@gmail.com>
2024-09-17 13:45:15 +0530

    methods

---
+++ practice/methods.java
@@ -0,0 +1,9 @@
+public class methods {
+  public static void main(String[] args) {
+  hello("sundar",26);
+  
+  }
+  static void hello(String name , int age){
+    System.out.println("hello " + name + " your age is " + age);
+  }
+}
